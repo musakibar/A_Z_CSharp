06dd4c8b9233e9f4a15f7b753dde0dbc870e20d3
musakibar <musakibar1108@hotmail.com>
2022-07-28 22:24:28 +0300

    TurDonusumuDuzenleme

--- ConsoleAppDeneme1/Program.cs
+++ ConsoleAppDeneme1/Program.cs
@@ -7,7 +7,7 @@ namespace Application
         {
 
             Console.WriteLine("Hello, World!");
-            //Console.WriteLine(args[0]);            
+            Console.WriteLine(args[0]);            
 
             #region Ek Bilgiler
 
@@ -18,70 +18,7 @@ namespace Application
 
             #endregion
 
-            #region Tür Dönüşümleri
-
-            #region Parse Metodu
-            // Parse metodu sadece string dataları hedef türe dönüştürürken kullanılır!
-            string x1 = "123";
-            //Console.WriteLine(short.Parse(x1) * 5);
-            short x2 = short.Parse(x1);
-            Console.WriteLine(x2 * 5);
-
-
-            #endregion
-            #region Convert Fonksiyonları
-            string x3 = "25";
-            int x4 = Convert.ToInt32(x3);
-
-
-            #endregion
-            #region ToString Fonksiyonu
-            //ToString fonksiyonu tüm türlerde mevcuttur.
-            int x5 = 456;
-            string y2 = x5.ToString();
-
-            #endregion
-
-            #region Bilinçsiz Tür Dönüşümü
-            //Bir sayısal türün kendisinden daha geniş aralıktaki başka bir türe atanması bilinçsiz tür dönüşümüdür.
-
-            //Örnek
-            short x6 = 123;
-            long y3 = x6;
-
-            #endregion
-            #region Bilinçli Tür Dönüşümü
-            // Bir sayısal türün kendisinden daha dar aralıkta başka türe atanması bilinçli tür dönüşümüdür.
-            // Veri kaybı olabileceği için sorumluluğu biz alırız.
-            int x7 = 4588;
-            short y4 = (short)x7;
-
-
-            #endregion
-            #region checked
-            //Bilinçli tür dönüşümü sırasında veri kaybı olacak olursa runtime'da uyarı verecek kontrol mekanizması.
-            checked
-            {
-                //int a1 = 500;
-                //byte b1 = (byte)a1;
-                //Console.WriteLine(b1);
-            }
-
-            #endregion
-            #region unchecked
-            //Bilinçli tür dönüşümü sırasında veri kaybı olacak olursa uyarı vermez.
-            unchecked
-            {
-                int a2 = 500;
-                byte b2 = (byte)a2;
-                Console.WriteLine(b2);
-            }
-
-
-            #endregion
-
-
-            #endregion
+            
 
 
 
---
+++ ConsoleAppDeneme1/TurDonusumleri.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Z_CSharp
+{
+    public class TurDonusumleri
+    {
+        static void _TurDonusumleri()
+        {
+            
+
+            #region Parse Metodu
+            // Parse metodu sadece string dataları hedef türe dönüştürürken kullanılır!
+            string x1 = "123";
+            //Console.WriteLine(short.Parse(x1) * 5);
+            short x2 = short.Parse(x1);
+            Console.WriteLine(x2 * 5);
+
+
+            #endregion
+            #region Convert Fonksiyonları
+            string x3 = "25";
+            int x4 = Convert.ToInt32(x3);
+
+
+            #endregion
+            #region ToString Fonksiyonu
+            //ToString fonksiyonu tüm türlerde mevcuttur.
+            int x5 = 456;
+            string y2 = x5.ToString();
+
+            #endregion
+
+            #region Bilinçsiz Tür Dönüşümü
+            //Bir sayısal türün kendisinden daha geniş aralıktaki başka bir türe atanması bilinçsiz tür dönüşümüdür.
+
+            //Örnek
+            short x6 = 123;
+            long y3 = x6;
+
+            #endregion
+            #region Bilinçli Tür Dönüşümü
+            // Bir sayısal türün kendisinden daha dar aralıkta başka türe atanması bilinçli tür dönüşümüdür.
+            // Veri kaybı olabileceği için sorumluluğu biz alırız.
+            int x7 = 4588;
+            short y4 = (short)x7;
+
+
+            #endregion
+            #region checked
+            //Bilinçli tür dönüşümü sırasında veri kaybı olacak olursa runtime'da uyarı verecek kontrol mekanizması.
+            checked
+            {
+                //int a1 = 500;
+                //byte b1 = (byte)a1;
+                //Console.WriteLine(b1);
+            }
+
+            #endregion
+            #region unchecked
+            //Bilinçli tür dönüşümü sırasında veri kaybı olacak olursa uyarı vermez.
+            unchecked
+            {
+                int a2 = 500;
+                byte b2 = (byte)a2;
+                Console.WriteLine(b2);
+            }
+
+
+            #endregion
+
+
+            
+        }
+
+
+    }
+}
